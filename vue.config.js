8441743642613d2f2e02b54548d72d7a86d82ba4
joanna.chen <ying0092@hotmail.com>
2020-07-30 10:57:44 +0800

    fix vue-config

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? '/test2/'
     : '/',
-  baseUrl: process.env.NODE_ENV === 'production'
-    ? './test2'
-    : '/'
+  outputDir: process.env.NODE_ENV === 'production'
+    ? 'dist'
+    : 'dist-dev',
 }
