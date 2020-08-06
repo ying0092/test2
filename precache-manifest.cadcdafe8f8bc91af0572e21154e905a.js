b85586271295c9fee03aeabfe91f9f3c434c1eea
joanna.chen <ying0092@hotmail.com>
2020-08-06 10:50:07 +0800

    deploy

---
+++ precache-manifest.cadcdafe8f8bc91af0572e21154e905a.js
@@ -0,0 +1,154 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c89925cda07be009bee528b8d234b9ac",
+    "url": "/test2/404.html"
+  },
+  {
+    "revision": "f63bbc2f22c2490114ce",
+    "url": "/test2/css/app.75e4697d.css"
+  },
+  {
+    "revision": "abb6096700343bf75c3d",
+    "url": "/test2/css/chunk-79291d46.c54f3492.css"
+  },
+  {
+    "revision": "9bb11c7f6b7880e22f62",
+    "url": "/test2/css/chunk-vendors.47543961.css"
+  },
+  {
+    "revision": "c6046eaf73259331916db1150e690c89",
+    "url": "/test2/favicon.png"
+  },
+  {
+    "revision": "928830b795a2a1cf596547150582af45",
+    "url": "/test2/img/ba.928830b7.jpg"
+  },
+  {
+    "revision": "458f2cbe9bb37732d278651114609be4",
+    "url": "/test2/img/bg-n1.458f2cbe.jpg"
+  },
+  {
+    "revision": "c9bfdc764f733ac9a36bb49331a4869b",
+    "url": "/test2/img/footer.c9bfdc76.svg"
+  },
+  {
+    "revision": "f130a0b70e386170cf6f011c0ca8c4f4",
+    "url": "/test2/img/icons 拷貝/android-chrome-192x192.png"
+  },
+  {
+    "revision": "0ff1bc4d14e5c9abcacba7c600d97814",
+    "url": "/test2/img/icons 拷貝/android-chrome-512x512.png"
+  },
+  {
+    "revision": "845a39478d0e2d4d5d32a092de2de250",
+    "url": "/test2/img/icons 拷貝/android-chrome-maskable-192x192.png"
+  },
+  {
+    "revision": "2695f5feb66cdb0c6f09d0e415824cf9",
+    "url": "/test2/img/icons 拷貝/android-chrome-maskable-512x512.png"
+  },
+  {
+    "revision": "936d6e411cabd71f0e627011c3f18fe2",
+    "url": "/test2/img/icons 拷貝/apple-touch-icon-120x120.png"
+  },
+  {
+    "revision": "1a034e64d80905128113e5272a5ab95e",
+    "url": "/test2/img/icons 拷貝/apple-touch-icon-152x152.png"
+  },
+  {
+    "revision": "c43cd371a49ee4ca17ab3a60e72bdd51",
+    "url": "/test2/img/icons 拷貝/apple-touch-icon-180x180.png"
+  },
+  {
+    "revision": "9a2b5c0f19de617685b7b5b42464e7db",
+    "url": "/test2/img/icons 拷貝/apple-touch-icon-60x60.png"
+  },
+  {
+    "revision": "af28d69d59284dd202aa55e57227b11b",
+    "url": "/test2/img/icons 拷貝/apple-touch-icon-76x76.png"
+  },
+  {
+    "revision": "66830ea6be8e7e94fb55df9f7b778f2e",
+    "url": "/test2/img/icons 拷貝/apple-touch-icon.png"
+  },
+  {
+    "revision": "ba284ee3c06244110088f8400bc195d1",
+    "url": "/test2/img/icons 拷貝/favicon-16x16.png"
+  },
+  {
+    "revision": "2918be270f9d045d23330c9d59bf1bd0",
+    "url": "/test2/img/icons 拷貝/favicon-32x32.png"
+  },
+  {
+    "revision": "b89032a4a5a1879f30ba05a13947f26f",
+    "url": "/test2/img/icons 拷貝/msapplication-icon-144x144.png"
+  },
+  {
+    "revision": "058a3335d15a3eb84e7ae3707ba09620",
+    "url": "/test2/img/icons 拷貝/mstile-150x150.png"
+  },
+  {
+    "revision": "f22d501a35a87d9f21701cb031f6ea17",
+    "url": "/test2/img/icons 拷貝/safari-pinned-tab.svg"
+  },
+  {
+    "revision": "4299f32a825e7efac64ca74512476012",
+    "url": "/test2/img/logo.4299f32a.svg"
+  },
+  {
+    "revision": "ce7e3562f096e6fef468345a904d53cb",
+    "url": "/test2/img/ying.ce7e3562.jpg"
+  },
+  {
+    "revision": "974f5736458d03ecbd8c0884831681e3",
+    "url": "/test2/index.html"
+  },
+  {
+    "revision": "f63bbc2f22c2490114ce",
+    "url": "/test2/js/app.37f3adc5.js"
+  },
+  {
+    "revision": "5a7d9cfe1802ab836b6d",
+    "url": "/test2/js/chunk-2d0bff30.2ef59e61.js"
+  },
+  {
+    "revision": "c77ef336a95724710215",
+    "url": "/test2/js/chunk-2d0c7b16.e8ba9263.js"
+  },
+  {
+    "revision": "d3114a7c4fb1eae885a6",
+    "url": "/test2/js/chunk-2d0cc602.2ca6809a.js"
+  },
+  {
+    "revision": "bc34f1dc7e21769d9215",
+    "url": "/test2/js/chunk-2d0d001a.a317d647.js"
+  },
+  {
+    "revision": "bcf45c450dbf0e1646b2",
+    "url": "/test2/js/chunk-2d0d9fe3.c8d8201d.js"
+  },
+  {
+    "revision": "5b062e50c9078131b222",
+    "url": "/test2/js/chunk-2d22fd44.62b2edaa.js"
+  },
+  {
+    "revision": "ab2cb1ad6740c74a2a76",
+    "url": "/test2/js/chunk-74bf1b8a.58bcd24a.js"
+  },
+  {
+    "revision": "abb6096700343bf75c3d",
+    "url": "/test2/js/chunk-79291d46.47d5b2c4.js"
+  },
+  {
+    "revision": "9bb11c7f6b7880e22f62",
+    "url": "/test2/js/chunk-vendors.c5757fdb.js"
+  },
+  {
+    "revision": "4d31a4dd8e5efc4f148671f916e18967",
+    "url": "/test2/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/test2/robots.txt"
+  }
+]);
